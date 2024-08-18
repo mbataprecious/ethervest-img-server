c4f0f990ab2b18500a1dc3db15849d0c2061d7fd
Precious Mbata <47004351+mbataprecious@users.noreply.github.com>
2024-08-18 02:59:06 +0100

    changes and update

--- routes/coinRoute.mjs
+++ routes/coinRoute.mjs
@@ -10,22 +10,36 @@ export const router = express.Router();
 
 
 
- const getQuoteData = async (req, res) => {
-const {query}=req
+const getQuoteData = async (req, res) => {
+  const { query } = req
   try {
 
     const url = 'https://api.coingecko.com/api/v3/coins/markets';
     const searchQuery = {
-        vs_currency: "usd",
+      vs_currency: "usd",
       ...query
     };
-    
+    const keyArray=['CG-mmxhLYJTDGm9dNb3uJSKkgn9','CG-Ya7462oMzxHmdvBxvCBhQSgs']
+// Generate a random index based on the array length
+const randomIndex = Math.floor(Math.random() * keyArray.length);
+
+// Use the random index to pick a random element
+const selectedKey = keyArray[randomIndex];
     const queryParams = new URLSearchParams(searchQuery);
-    const fullUrl = `${url}?${queryParams}`;
-   console.log({fullUrl}) 
-  const fetchRes= await fetch(fullUrl)
-  console.log({fetchRes})
-      const data= await fetchRes.json()   
+    const fullUrl = `${url}?${queryParams.toString()}`;
+    console.log({ fullUrl })
+    const fetchRes = await fetch(fullUrl,{
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-cg-demo-api-key': selectedKey,
+      },
+    })
+    console.log({ fetchRes })
+    if (!fetchRes.ok) {
+      throw new Error(`HTTP error! status: ${fetchRes.status}`);
+    }
+    const data = await fetchRes.json()
     // console.log({mappedData})
     response(res, 200, "fetched successfully", data);
   } catch (err) {
@@ -36,6 +50,6 @@ const {query}=req
 
 
 
-router.get("/markets", getQuoteData );
+router.get("/markets", getQuoteData);
 
 
